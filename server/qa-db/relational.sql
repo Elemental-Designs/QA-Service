a041c692f2bc5ad1d8a38b9404084cefa7ce6d7c
Jerome Rodriguez <jeromemtrodriguez@gmail.com>
2022-07-28 15:54:41 -0700

    refactored mongodb schema

--- server/qa-db/relational.sql
+++ server/qa-db/relational.sql
@@ -1,39 +1,39 @@
 CREATE TABLE product (
-  id INT PRIMARY KEY,
+  id INTEGER PRIMARY KEY,
   UNIQUE(id)
 );
 
 CREATE TABLE questions (
-  id INT PRIMARY KEY NOT NULL,
-  product_id INT NOT NULL,
+  id INTEGER PRIMARY KEY NOT NULL,
+  product_id INTEGER NOT NULL,
   body VARCHAR(255) NOT NULL,
   date_written DATE NOT NULL,
   asker_name VARCHAR(100) NOT NULL,
   asker_email VARCHAR(50) NOT NULL,
-  question BOOLEAN,
-  helpful INT NOT NULL,
+  reported BOOLEAN,
+  helpful INTEGER NOT NULL,
   CONSTRAINT fk_product
     FOREIGN KEY(product_id)
       REFERENCES product(id)
 );
 
 CREATE TABLE answers (
-  id INT PRIMARY KEY NOT NULL,
-  questions_id INT NOT NULL,
+  id INTEGER PRIMARY KEY NOT NULL,
+  questions_id INTEGER NOT NULL,
   body VARCHAR(255) NOT NULL,
   date_written DATE NOT NULL,
   answer_name VARCHAR(100) NOT NULL,
   answer_email VARCHAR(50) NOT NULL,
   reported BOOLEAN,
-  helpful INT NOT NULL,
+  helpful INTEGER NOT NULL,
   CONSTRAINT fk_question
     FOREIGN KEY(questions_id)
       REFERENCES questions(id)
 );
 
 CREATE TABLE answers_photo (
-  id INT PRIMARY KEY NOT NULL,
-  answers_id INT NOT NULL,
+  id INTEGER PRIMARY KEY NOT NULL,
+  answers_id INTEGER NOT NULL,
   url VARCHAR(150),
   CONSTRAINT fk_answers
     FOREIGN KEY(answers_id)
