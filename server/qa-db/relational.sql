c41a02c71754a3c3f775d747e7f4575e362bd355
Jerome Rodriguez <jeromemtrodriguez@gmail.com>
2022-07-28 17:59:18 -0700

    completed etl for product schema

--- server/qa-db/relational.sql
+++ server/qa-db/relational.sql
@@ -1,5 +1,18 @@
+DROP TABLE product, questions, answers, answers_photo
+
+CREATE TABLE temp (
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(100),
+  slogan VARCHAR(255),
+  description TEXT,
+  category VARCHAR(50),
+  default_price INTEGER,
+  UNIQUE(id)
+);
+
 CREATE TABLE product (
   id INTEGER PRIMARY KEY,
+  name VARCHAR(100),
   UNIQUE(id)
 );
 
@@ -7,7 +20,7 @@ CREATE TABLE questions (
   id INTEGER PRIMARY KEY NOT NULL,
   product_id INTEGER NOT NULL,
   body VARCHAR(255) NOT NULL,
-  date_written DATE NOT NULL,
+  date_written BIGINT NOT NULL,
   asker_name VARCHAR(100) NOT NULL,
   asker_email VARCHAR(50) NOT NULL,
   reported BOOLEAN,
@@ -21,7 +34,7 @@ CREATE TABLE answers (
   id INTEGER PRIMARY KEY NOT NULL,
   questions_id INTEGER NOT NULL,
   body VARCHAR(255) NOT NULL,
-  date_written DATE NOT NULL,
+  date_written BIGINT NOT NULL,
   answer_name VARCHAR(100) NOT NULL,
   answer_email VARCHAR(50) NOT NULL,
   reported BOOLEAN,
@@ -39,3 +52,14 @@ CREATE TABLE answers_photo (
     FOREIGN KEY(answers_id)
       REFERENCES answers(id)
 );
+
+\COPY temp FROM 'qa_csv/product.csv' DELIMITER ',' CSV HEADER;
+
+INSERT INTO product(id, name)
+SELECT id, name
+FROM temp;
+
+DROP TABLE temp;
+
+\COPY questions 'qa_csv/questions.csv' DELIMITER ',' CSV HEADER;
+SELECT COUNT(*) FROM questions;
