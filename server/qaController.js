3a50310bb549f698e5a4db04b668576a50f3248d
Jerome Rodriguez <jeromemtrodriguez@gmail.com>
2022-08-01 23:12:17 -0700

    added error handling for invalid params, implemented queries for endpoints 90% complete

--- server/qaController.js
+++ server/qaController.js
@@ -16,29 +16,34 @@ module.exports = {
 
   // get list of questions (/questions)
   getQuestions(req, res) {
+    if (!req.query.product_id) handleError(res, 'invalid product_id')
     console.log(req.query);
     let params = {
       product_id: req.query.product_id,
       page: req.params.page || 1,
-      count: req.params.count || 5,
+      count: req.params.count || 10,
     }
     readQuestions(params)
     .then(({rows})=> handleResponse(res, rows[0], 200))
-    .catch(err => handleError(res, err));
+    .catch(err => console.log(err));
   },
 
   // get list of answers from specific question (:question_id/answers)
   getAnswers(req, res) {
+    if (!req.params.question_id) handleError(res, 'invalid question_id')
     let params = {
       question_id: req.params.question_id,
       page: req.query.page || 1,
-      count: req.qeury.count || 5
+      count: req.query.count || 5
     }
     readAnswers(params)
+      .then(({rows}) => handleResponse(res, rows[0], 200))
+      .catch(err => handleError(res, err));
   },
 
   // post question for product (/questions)
   postQuestion(req, res) {
+    if (!req.body.product_id) handleError(res, 'invalid product_id')
     // get date and add to req body
     insertQuestion(req.body)
       .then(result => handleResponse(res, null, 201))
@@ -47,9 +52,11 @@ module.exports = {
 
   // post ansewr for specific question (/questions/:question_id/answers)
   postAnswer(req, res) {
+    if (!req.params.question_id) handleError(res, 'invalid question_id')
     let params = {
       ...req.body,
-      ...req.params
+      ...req.params,
+      ...req.query
     }
     insertAnswer(params)
       .then(result => handleResponse(res, null, 201))
@@ -58,6 +65,7 @@ module.exports = {
 
   // PUT/UPDATE mark the question as helpful (/questions/:question_id/helpful)
   markQuestionHelpful(req, res) {
+    if (!req.params.question_id) handleError(res, 'invalid question_id')
     updateQuestionHelpful(req.params.question_id)
       .then(result => handleResponse(res, null, 204))
       .catch(err => handleError(res, err));
@@ -65,6 +73,7 @@ module.exports = {
 
   // PUT/UPDATE mark the question as reported (/questions/:question_id/report)
   reportQuestion(req, res) {
+    if (!req.params.question_id) handleError(res, 'invalid question_id')
     updateReportQuestion(req.params.question_id)
       .then(result => handleResponse(res, null, 204))
       .catch(err => handleError(res, err));
@@ -72,6 +81,7 @@ module.exports = {
 
   // PUT/UPDATE mark the answer as helpful (/questions/:answer_id/helpful)
   markAnswerHelpful(req, res) {
+    if (!req.params.answer_id) handleError(res, 'invalid answer_id')
     updateAnswerHelpful(req.params.answer_id)
       .then(result => handleResponse(res, null, 204))
       .catch(err => handleError(res, err));
@@ -79,6 +89,7 @@ module.exports = {
 
   // PUT/UPDATE mark the answer as reported (/questions/:answer_id/report)
   reportAnswer(req, res) {
+    if (!req.params.answer_id) handleError(res, 'invalid answer_id')
     updateReportAnswer(req.params.answer_id)
       .then(result => handleResponse(res, null, 204))
       .catch(err => handleError(res, err));
--- server/qaModel.js
+++ server/qaModel.js
@@ -3,38 +3,98 @@ const query = require('./qa-db');
 module.exports = {
 
   readQuestions({ product_id, page, count }) {
-    console.log('read')
     const text = `
       SELECT product_id,
       (
-        SELECT JSON_AGG
+        json_agg
         (
-          JSON_BUILD_OBJECT
+          json_build_object
           (
             'id', q.id,
             'body', q.body,
+            'date', (SELECT TO_CHAR(to_timestamp(q.date_written / 1000), 'YYYY-MM-DD"T"HH24:MI:SS:000"Z"')),
             'asker_name', q.asker_name,
             'question_helpfulness', q.helpful,
-            'reported', q.reported
+            'reported', q.reported,
+            'answers',
+            (
+              SELECT json_object_agg
+              (
+                id, json_build_object
+                (
+                  'id', a.id,
+                  'body', a.body,
+                  'date', (SELECT TO_CHAR(to_timestamp(a.date_written / 1000), 'YYYY-MM-DD"T"HH24:MI:SS:000"Z"')),
+                  'answerer', a.answer_name,
+                  'helpfulness', a.helpful,
+                  'photos',
+                  (
+                    SELECT coalesce
+                    (
+                      json_agg(ap.url),'[]'
+                    )
+                    FROM answers_photos AS ap
+                    WHERE ap.answers_id = a.id
+                  )
+                )
+              )
+              FROM answers AS a
+              WHERE a.questions_id = q.id
+            )
           )
         )
-        FROM questions AS q WHERE q.product_id = $1
       ) AS results
-      FROM questions
-      WHERE questions.product_id = $1
+      FROM questions AS q
+      WHERE q.product_id = $1
+      GROUP BY 1
+      OFFSET $2
+      LIMIT $3
     `;
-    const values = [product_id];
-
+    const offset = Number(page) * Number(count) - count;
+    const values = [product_id, offset, count];
     return query(text, values);
   },
 
-
   readAnswers({question_id, page, count}){
+    console.log('read q')
     const text = `
-      SELECT * FROM answers WHERE reported = false LIMIT 2
-    `;
-    const values = [product_id, page, count];
+      SELECT questions_id, $2 AS page, $3 AS count,
+      (
+        json_agg
+        (
+          json_build_object
+          (
+            'answer_id', a.id,
+            'body', a.body,
+            'date', (SELECT TO_CHAR(to_timestamp(a.date_written / 1000), 'YYYY-MM-DD"T"HH24:MI:SS:000"Z"')),
+            'answerer_name', a.answer_name,
+            'helpfulness', a.helpful,
+            'photos',
+            (
+              SELECT coalesce
+              (
+                json_agg
+                (
+                  json_build_object
+                  (
+                    'id', ap.id,
+                    'url', ap.url
+                  )
+                ), '[]'
+              )
+              FROM answers_photos AS ap
+              WHERE ap.answers_id = a.id
+            )
+          )
+        )
+      ) AS results
+      FROM answers AS a
+      WHERE a.questions_id = $1
+      GROUP BY 1
 
+    `;
+    const offset = (parseInt(page) - 1) * parseInt(count);
+    const values = [question_id, page, count,];
     return query(text, values);
   },
 
@@ -49,12 +109,15 @@ module.exports = {
   },
 
 
-  insertAnswer() {
+  insertAnswer({question_id, body, date,name, email, photos}) {
     const text =`
       INSERT INTO answers(question_id, body, date_written, answer_name, answer_email)
+      RETURNING id
     `;
-    const values = [id];
-    return query(text, values);
+    const values = [question_id, body, /* get date */, name, email];
+    const photoText = `INSERT INTO answers_photo(answers_id, url) VALUES ($1, $2)`
+    return query(text, values)
+      .then((id) => query(photoText, [photos]))
   },
 
 
