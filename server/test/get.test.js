f3e3311b34f8cda707a9b594acb5b35f8070a716
Jerome Rodriguez <jeromemtrodriguez@gmail.com>
2022-08-03 10:53:44 -0700

    implementing get test for answers and questions

--- server/test/get.test.js
+++ server/test/get.test.js
@@ -5,6 +5,8 @@ describe("GET /questions", function () {
   it("returns questions from product id, limited to 5 per page", async function () {
     const response = await request.get("/questions");
 
+    expect(response.status).to.eql(200);
+
 
 
   });
@@ -15,6 +17,7 @@ describe("GET /questions/:question/answers", function () {
   it("returns all answers, limited to 5 per page", async function () {
     const response = await request.get("/questions/1/answers");
 
+    expect(response.status).to.eql(200);
 
   });
 });
