ed297fad7e95acf6dcc7a6ccc360efdf5dda2298
Jerome Rodriguez <jeromemtrodriguez@gmail.com>
2022-08-03 03:44:49 -0700

    completed tests for put

--- server/test/put.test.js
+++ server/test/put.test.js
@@ -1,2 +1,30 @@
 const request = require('supertest')('http://localhost:3000/qa');
 const expect = require('chai').expect;
+
+describe("PUT /questions/:question_id/helpful",()=>{
+  it("should update a question to show it was found helpful", async function(){
+    const response = await request.put(`/questions/1/helpful`);
+    expect(response.status).to.eql(204);
+  });
+});
+
+describe("PUT /questions/:question_id/report",()=>{
+  it("should update a question to show it was found helpful", async function(){
+    const response = await request.put(`/questions/1/report`);
+    expect(response.status).to.eql(204);
+  });
+});
+
+describe("PUT /answers/:answer_id/helpful",()=>{
+  it("should update a question to show it was found helpful", async function(){
+    const response = await request.put(`/answers/1/helpful`);
+    expect(response.status).to.eql(204);
+  });
+});
+
+describe("PUT /answers/:answer_id/report",()=>{
+  it("should update a question to show it was found helpful", async function(){
+    const response = await request.put(`/answers/1/report`);
+    expect(response.status).to.eql(204);
+  });
+});
