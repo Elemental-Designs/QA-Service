670c803bd12653d8d0bdb7ebe09467fa99928833
Jerome Rodriguez <jeromemtrodriguez@gmail.com>
2022-08-05 13:25:50 -0700

    implemented process.env

--- qa-db/index.js
+++ qa-db/index.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'jeromerodriguez',
-  host: 'localhost',
-  database: 'qa',
-  password: 'psql',
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDB,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT
 });
 
 module.exports = (text, values) => (
